bb4a8d006008199b8bd1df2d274a8b9380b2ad48
Matteo <matteo.lupini5@gmail.com>
2019-03-15 15:12:49 +0100

    Refactoring

--- src/main.js
+++ src/main.js
@@ -4,37 +4,36 @@ const numbers = [5,10,15];
 
 //selettore di d3
 d3.select('#app')
-  //dentro al container app appendi h1
+//dentro al container app appendi h1
   .append('h1')
   //con testo inerente
   .text('My first d3.js application')
 
-
 const ul = d3.select('#app')
   .append('ul')
 
+function redraw() {
 //select all because we have multiple elements
-let lis = ul.selectAll('li')
+  let lis = ul.selectAll('li')
   //andiamo a specificare dentro o parametri di data i dati a disposizione
-  .data(numbers)
+    .data(numbers)
 
 //    exit
 //rimuovo gli elementi visuali che non interessano i dati che ho
-lis.exit().remove();
+  lis.exit().remove();
 
 //    enter
-lis = lis.enter()
-  .append('li')
-  //effettuo il merge con le cose che c'erano prima.
-  .merge(lis);
+  lis = lis.enter()
+    .append('li')
+    //effettuo il merge con le cose che c'erano prima.
+    .merge(lis);
 
 //    update
 //lis.text(function (d, i) {
 //  return 'Element numbers is ' + d;
 //});
-lis.text((d,i) => ('Element number is '+ d));
-
-
+  lis.text((d, i) => ('Element number is ' + d));
+}
 
 d3.select('#btnAdd')
   .on('click', function () {
@@ -46,3 +45,4 @@ d3.select('#btnRemove')
     console.log('Remove a number');
   });
 
+redraw();
